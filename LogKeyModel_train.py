0f5b1d0e28f2143a0fd80b770a18005b7f22b70f
Juri Dispan <juri.dispan@uni-ulm.de>
2022-03-28 12:42:38 +0200

    cpu at debug

--- LogKeyModel_train.py
+++ LogKeyModel_train.py
@@ -8,7 +8,7 @@ import argparse
 import os
 
 # Device configuration
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu") #  torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def generate(name):
